8ee00d49fdb747affd52460d18b73f0ed017ca40
ritu-7999 <81248835+ritu-7999@users.noreply.github.com>
2023-06-18 17:39:37 +0530

    width changes in exchange card

--- src/Components/ExchangeCard.jsx
+++ src/Components/ExchangeCard.jsx
@@ -10,7 +10,7 @@ function ExchangeCard({name,img,url,rank }) {
                     transform: "scale(1.1)",
                 },
             }}>
-                <Image src={img} w={"10"} h={"10"} objectFit={"contain"} alt={"Exchange"} />
+                <Image src={img} w={"20"} h={"20"} objectFit={"contain"} alt={"Exchange"} />
                 <Heading size={"md"} noOfLines={1}>{rank}</Heading>
                 <Text noOfLines={1}>{name}</Text>
             </VStack>
